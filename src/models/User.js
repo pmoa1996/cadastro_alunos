d507997b6811572d4500f37e73fe0d02912966f4
pmoa <paulomatheusoa@gmail.com>
2023-12-29 14:02:26 -0300

    1 - Criei uma migration para usuário com as colunas de id, nome, email, password_rash, created_at e updated_at. 2 - Criei um model User com as autenticações de nome, email e password. E fiz uma função para que o password fosse hasheado. 3 - Fiz uma alteração nas regras do eslint, para permitir o uso de um parâmetro: 'no-param-reassign': 'off'.

---
+++ src/models/User.js
@@ -0,0 +1,49 @@
+import Sequelize, { Model } from 'sequelize';
+import bcryptjs from 'bcryptjs';
+
+export default class User extends Model {
+  static init(sequelize) {
+    super.init({
+      nome: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [4, 28],
+            msg: 'Campo nome deve ter entre 4 e 28 caracteres',
+          },
+        },
+      },
+      email: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          isEmail: {
+            msg: 'E-mail inválido',
+          },
+        },
+      },
+      password_hash: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+      },
+      password: {
+        type: Sequelize.VIRTUAL,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [6, 50],
+            msg: 'A senha precisa ter entre 6 e 50 caracteres',
+          },
+        },
+      },
+    }, {
+      sequelize,
+    });
+
+    this.addHook('beforeSave', async (user) => {
+      user.password_hash = await bcryptjs.hash(user.password, 8);
+    });
+    return this;
+  }
+}
