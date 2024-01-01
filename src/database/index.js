72e070991807a964e402047fdb5f34f71e97263a
pmoa <paulomatheusoa@gmail.com>
2024-01-01 14:25:34 -0300

    Mudança no database/index não foi enviada anteriormente

--- src/database/index.js
+++ src/database/index.js
@@ -3,7 +3,7 @@ import databaseConfig from '../config/database';
 import Aluno from '../models/Aluno';
 import User from '../models/User';
 
-const models = [Aluno, User]; // Caso houvessem outros models, eles viriam para este array
+const models = [Aluno, User];
 
 const connection = new Sequelize(databaseConfig);
 
