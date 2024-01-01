59d3a53bd473ba1fab4e35901c7adba45bb0be21
pmoa <paulomatheusoa@gmail.com>
2024-01-01 15:37:01 -0300

    1 -> Implementados métodos CRUD no UserController para gerenciar usuários. Os métodos 'index', 'show', 'update' e 'delete' possibilitam listar todos os usuários, exibir detalhes de um usuário específico, atualizar informações e deletar um usuário, respectivamente. Além disso, o método 'store' foi aprimorado para criar um novo usuário com validações e retornar mensagens de erro adequadas, se necessário.
    2 -> Adicionada validação de criptografia de senha antes do armazenamento no banco de dados, no model User.
    
    3 -> Adicionadas rotas no userRoutes para os métodos do CRUD do UserController.

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,12 +1,86 @@
 import User from '../models/User';
 
 class UserController {
+  // Store
   async store(req, res) {
     try {
-      const novoUser = await User.create(req.body); // descrever isso aqui!
-      res.json(novoUser);
+      const novoUser = await User.create(req.body);
+      return res.json(novoUser);
     } catch (error) {
-      res.status(400).json({
+      return res.status(400).json({
+        errors: error.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  // Index
+  async index(req, res) {
+    try {
+      const users = await User.findAll();
+      return res.json(users);
+    } catch (error) {
+      return res.json(null);
+    }
+  }
+
+  // Show
+  async show(req, res) {
+    try {
+      const user = await User.findByPk(req.params.id);
+      return res.json(user);
+    } catch (error) {
+      return res.json(null);
+    }
+  }
+
+  // Update
+  async update(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(400).json({
+          errors: ['ID não enviado.'],
+        });
+      }
+
+      const user = await User.findByPk(req.params.id);
+
+      if (!user) {
+        return res.status(400).json({
+          errors: ['Usuário não existe.'],
+        });
+      }
+
+      const newData = await user.update(req.body);
+
+      return res.json(newData);
+    } catch (error) {
+      return res.status(400).json({
+        errors: error.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  // Delete
+  async delete(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(400).json({
+          errors: ['ID não enviado.'],
+        });
+      }
+
+      const user = await User.findByPk(req.params.id);
+
+      if (!user) {
+        return res.status(400).json({
+          errors: ['Usuário não existe.'],
+        });
+      }
+
+      await user.destroy();
+      return res.json(user);
+    } catch (error) {
+      return res.status(400).json({
         errors: error.errors.map((err) => err.message),
       });
     }
