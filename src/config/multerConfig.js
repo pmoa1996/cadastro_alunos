a751e7e85e1e886f82468d0148e78b417a481ac7
pmoa <paulomatheusoa@gmail.com>
2024-01-03 21:54:49 -0300

    Refatoração do Upload de Fotos:
    - PhotoController: Atualização do método `store` para utilizar a função `upload` do multer no processamento do armazenamento. Adicionada manipulação de erros para retornar códigos de erro adequados em caso de falha no upload.
    
    - multerConfig: Adicionada validação de tipo de arquivo (`fileFilter`) para aceitar apenas formatos PNG, JPEG e JPG.
    
    - photoRoutes: Não houve alterações significativas, mantendo a rota POST para o controlador `PhotoController`.
    
    Essas mudanças visam melhorar a funcionalidade de upload de fotos, implementando validações de tipo de arquivo e organizando o processamento de armazenamento.

--- src/config/multerConfig.js
+++ src/config/multerConfig.js
@@ -4,10 +4,22 @@ import { extname, resolve } from 'path';
 const aleatorio = () => Math.floor(Math.random() * 10000 + 10000);
 
 export default {
+  fileFilter: (req, file, cb) => {
+    // Validação para arquivos com formatos diferentes
+    if (file.mimetype !== 'image/png' && file.mimetype !== 'image/jpeg' && file.mimetype !== 'image/jpg') {
+      return cb(new multer.MulterError('Arquivo precisa ser PNG, JPEG ou JPG'));
+    }
+
+    return cb(null, true);
+  },
+
+  // Configuração do multer para armazenamento de arquivos
   storage: multer.diskStorage({
+    // Define o diretório de destino para os uploads
     destination: (req, file, cb) => {
       cb(null, resolve(__dirname, '..', '..', 'uploads'));
     },
+    // Define o nome do arquivo a ser salvo
     filename: (req, file, cb) => {
       cb(null, `${Date.now()}_${aleatorio()}${extname(file.originalname)}`);
     },
